de3d2e035cf1c5b3d278377e5bccc83bd5e0f7ba
Скородумов Сергей <pprofisional41@gmail.com>
2025-12-04 21:08:15 +0300

    [REF] : [MAIN] Сводим ветки

--- huur.py
+++ huur.py
@@ -1,33 +1,4 @@
 while True:
     numbers = input("Введите число знак число: ")
-    parts = numbers
-    if len(parts) != 3:
-        print("Вы вввели не допустимый формат строки!")
-    else:
-        num_1 = int(parts[0])
-        sign = (parts[1])
-        num_2 = int(parts[2])
-        if sign == "+":
-            result = num_1 + num_2
-            print(f"{num_1} + {num_2} = {result}")
-        elif sign == "-":
-            result = num_1 - num_2
-            print(f"{num_1} - {num_2} = {result}")
-        elif sign == "*":
-            result = num_1 * num_2
-            print(f"{num_1} * {num_2} = {result}")
-        elif sign == "/":
-            result = num_1 / num_2
-            if num_1 == 0:
-                print("Делить на 0 нельзя!")
-            else:
-                print(f"{num_1} / {num_2} = {result}")
-        elif sign == "^":
-            result = num_1 ^ num_2
-            print(f"{num_1} ^ {num_2} = {result}")
-
-
-
-
-
-
+    result = eval(numbers)
+    print(f"{result}")
